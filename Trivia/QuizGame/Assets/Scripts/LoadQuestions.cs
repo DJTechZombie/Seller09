25d6301115bbc48d00dd22972937d9de770fe4e9
DJTechZombie <46727082+DJTechZombie@users.noreply.github.com>
2019-02-04 23:42:17 -0600

    Add CSV reader

---
+++ Trivia/QuizGame/Assets/Scripts/LoadQuestions.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+public class LoadQuestions {
+    public TextAsset csvFile;
+    int i = 0;    
+
+
+    public static StreamReader LoadQuestion()
+    {
+        Debug.Log("Start of Log");
+        string path = "Assets/Questions/trivia.csv";
+
+        if(path == null)
+        {
+            Debug.Log("Trivia.CSV not found");
+        }
+        else
+        if(path != null)
+        {
+            Debug.Log("Trivia.CSV file found.");
+        }
+        var _question = new List<string>();
+        var _answerA = new List<string>();
+        var _answerB = new List<string>();
+        var _answerC = new List<string>();
+        var _answerD = new List<string>();
+        var _correct = new List<string>();
+        var _value = new List<string>();
+        var _category = new List<string>();
+
+        StreamReader sr = new StreamReader(path);
+        return sr;
+        /*using (StreamReader sr = new StreamReader(path))
+        {
+            /*while(! sr.EndOfStream)
+            {
+               var split = sr.ReadLine().Split(',');
+               _question.Add(split[0]);
+               _answerA.Add(split[1]);
+               _answerB.Add(split[2]);
+               _answerC.Add(split[3]);
+               _answerD.Add(split[4]);
+               _correct.Add(split[5]);
+               _value.Add(split[6]);
+               _category.Add(split[7]);
+            }
+       
+            return sr;
+      }
+      */
+        
+    }
+}
---
+++ Trivia/QuizGame/Assets/Scripts/Question.cs
@@ -0,0 +1,30 @@
+﻿//Jeremy Sellers
+//Game Programing
+
+[System.Serializable]
+
+public class Question {
+
+    public string fact;
+    public string answerA;
+    public string answerB;
+    public string answerC;
+    public string answerD;
+    public string value;
+    public string correctAnswer;
+    public string category;
+
+
+    public Question (string[] inStream)
+    {
+        //string[] inStream = _questions.Split(',');
+        this.fact = inStream[0];
+        this.answerA = inStream[1];
+        this.answerB = inStream[2];
+        this.answerC = inStream[3];
+        this.answerD = inStream[4];
+        this.correctAnswer = inStream[5];
+        this.value = inStream[6];
+        this.category = inStream[7];
+    }
+}
